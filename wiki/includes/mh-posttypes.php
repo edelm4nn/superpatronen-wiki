43ab53162d5418abc4726212a2735a63406f4f41
Antonio <post@antonio-leutsch.com>
2017-11-07 12:14:20 +0100

    wiki post type

--- wiki/includes/mh-posttypes.php
+++ wiki/includes/mh-posttypes.php
@@ -19,20 +19,19 @@ function cptui_register_my_cpts() {
 		"show_ui" => true,
 		"show_in_rest" => false,
 		"rest_base" => "",
-		"has_archive" => "wiki",
 		"show_in_menu" => true,
 		"exclude_from_search" => false,
 		"capability_type" => "post",
 		"map_meta_cap" => true,
 		"hierarchical" => false,
-		"rewrite" => array( "slug" => "wiki", "with_front" => true ),
+		"rewrite" => array( "slug" => "wiki", "with_front" => false ),
 		"query_var" => true,
 		"menu_position" => 5,
 		"menu_icon" => "dashicons-welcome-learn-more",
 		"supports" => array( "title", "editor", "thumbnail" ),
 	);
 
-	register_post_type( "wiki", $args );
+	register_post_type( "wiki-entry", $args );
 }
 
 add_action( 'init', 'cptui_register_my_cpts' );
